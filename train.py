4905309960d6c596b29198862228993cf23de3a5
tahsin314 <adarswapnik3.14@gmail.com>
2019-06-07 19:50:11 +0600

    Image changed from grayscale to 3 channel

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ train_label = []
 for i in train_files:
     train_data.append(cv2.resize((cv2.imread(training_images_loc + i)), (512, 512)))
     # Change '_manual1.tiff' to the label name
-    temp = cv2.resize(cv2.imread(training_label_loc + i.split('_')[0] + '_manual1.tiff', cv2.IMREAD_GRAYSCALE),
+    temp = cv2.resize(cv2.imread(training_label_loc + i.split('_')[0] + '_manual1.tiff'),
                       (512, 512))
     _, temp = cv2.threshold(temp, 127, 255, cv2.THRESH_BINARY)
     train_label.append(temp)
@@ -39,7 +39,7 @@ test_label = []
 for i in test_files:
     test_data.append(cv2.resize((cv2.imread(testing_images_loc + i)), (512, 512)))
     # Change '_manual1.tiff' to the label name
-    temp = cv2.resize(cv2.imread(testing_label_loc + i.split('_')[0] + '_manual1.tiff', cv2.IMREAD_GRAYSCALE),
+    temp = cv2.resize(cv2.imread(testing_label_loc + i.split('_')[0] + '_manual1.tiff'),
                       (512, 512))
     _, temp = cv2.threshold(temp, 127, 255, cv2.THRESH_BINARY)
     test_label.append(temp)
@@ -48,21 +48,19 @@ test_label = np.array(test_label)
 
 x_train = train_data.astype('float32') / 255.
 y_train = train_label.astype('float32') / 255.
-x_train = np.reshape(x_train, (len(x_train), 512, 512, 1))  # adapt this if using `channels_first` image data format
+x_train = np.reshape(x_train, (len(x_train), 512, 512, 3))  # adapt this if using `channels_first` image data format
 y_train = np.reshape(y_train, (len(y_train), 512, 512, 1))  # adapt this if using `channels_first` im
 
 x_test = test_data.astype('float32') / 255.
 y_test = test_label.astype('float32') / 255.
-x_test = np.reshape(x_test, (len(x_test), 512, 512, 1))  # adapt this if using `channels_first` image data format
+x_test = np.reshape(x_test, (len(x_test), 512, 512, 3))  # adapt this if using `channels_first` image data format
 y_test = np.reshape(y_test, (len(y_test), 512, 512, 1))  # adapt this if using `channels_first` im
 
 TensorBoard(log_dir='./autoencoder', histogram_freq=0,
             write_graph=True, write_images=True)
 
 
-
-# model = unet()
-model = residual_unet()
+model = residual_unet((512, 512, 3))
 
 if os.path.isfile('resnet_retinal_vessel.hdf5'): model.load_weights('resnet3_retinal_vessel.hdf5')
 
